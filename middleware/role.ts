5c1def4a86b936a74268060141790ac9d3411dec
David Osuna <davidosuna1987@gmail.com>
2023-08-03 06:30:30 +0200

    refactor: prevent infinite redirection

--- middleware/role.ts
+++ middleware/role.ts
@@ -4,7 +4,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const auth = useAuthStore()
 
   if (!auth.user) {
-    if (from) {
+    if (from && from.path !== to.path) {
       return navigateTo(from.path)
     } else if (to.path !== '/') {
       return navigateTo('/')
