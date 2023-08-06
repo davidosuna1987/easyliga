71dc3b8f47ae6f859b71158cea4f89dca911ce81
David Osuna <davidosuna1987@gmail.com>
2023-08-06 11:37:06 +0200

    feat: created navbar component

---
+++ components/Navbar/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/useAuthStore'
+const auth = useAuthStore()
+</script>
+
+<template>
+  <NavbarAuth v-if="auth.isLoggedIn" />
+  <NavbarGuest v-else />
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,6 @@
-<script setup lang="ts">
-import { useAuthStore } from '@/stores/useAuthStore'
-import { User } from '@/types/api/auth'
-
-const auth = useAuthStore()
-const authuser = computed((): User | null => auth.user)
-</script>
-
 <template>
   <div class="default-layout">
-    <NavbarAuth v-if="authuser" />
-    <NavbarGuest v-else />
+    <NavbarApp />
 
     <main class="easy-main">
       <section class="easy-container">
