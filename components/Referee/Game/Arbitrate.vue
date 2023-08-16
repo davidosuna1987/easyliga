2e7df28df8382648eebe29a68f20e32b136a25d9
David Osuna <davidosuna1987@gmail.com>
2023-08-16 06:30:03 +0200

    feat: team sets won component mock created

---
+++ components/Referee/Game/Arbitrate.vue
@@ -0,0 +1,100 @@
+<script lang="ts" setup>
+import { GameInitialData, mapApiGameInitialDataToGame } from '@/domain/game'
+import { Call, mapApiCallToCall } from '@/domain/call'
+import GameService from '@/services/game'
+import { ApiErrorObject } from '@/types/errors'
+import { ApiCallUpdatedEventResponse } from '@/types/api/call'
+
+const route = useRoute()
+const toast = useEasyToast()
+const gameService = new GameService()
+
+const gameInitialData = ref<GameInitialData>()
+const localTeamCall = ref<Call>()
+const visitorTeamCall = ref<Call>()
+const loadingApi = ref<boolean>(false)
+const errors = ref<ApiErrorObject | null>(null)
+
+const getGameInitialData = async () => {
+  loadingApi.value = true
+  const { data, error } = await gameService.initialData(
+    Number(route.params.game_id),
+  )
+
+  if (error.value || !data.value) {
+    toast.mapError(Object.values(error.value?.data?.errors))
+    errors.value = error.value?.data?.errors
+    return
+  }
+
+  gameInitialData.value = mapApiGameInitialDataToGame(data.value)
+  localTeamCall.value = gameInitialData.value?.calls?.find(
+    call => call.teamId === gameInitialData.value?.localTeam?.id,
+  )
+  visitorTeamCall.value = gameInitialData.value?.calls?.find(
+    call => call.teamId === gameInitialData.value?.visitorTeam?.id,
+  )
+  loadingApi.value = false
+}
+
+const listenCallUpdatedEvent = () => {
+  window.Echo.channel(`game.${route.params.game_id}.call.updated`).listen(
+    'CallUpdatedEvent',
+    (response: ApiCallUpdatedEventResponse) => {
+      toast.info(
+        useNuxtApp().$i18n.t('events.call_updated', {
+          teamName: response.team.name,
+        }),
+        {
+          life: 300000,
+        },
+      )
+      if (response.team.id === gameInitialData.value?.localTeam?.id) {
+        localTeamCall.value = mapApiCallToCall(response.call)
+      } else {
+        visitorTeamCall.value = mapApiCallToCall(response.call)
+      }
+    },
+  )
+}
+
+onBeforeMount(() => {
+  getGameInitialData()
+})
+
+onMounted(() => {
+  listenCallUpdatedEvent()
+})
+</script>
+
+<template>
+  <div class="easy-referee-game-arbitrate-component">
+    <RefereeGameTeamSetsWon
+      v-if="gameInitialData"
+      :localTeam="gameInitialData?.localTeam"
+      :visitorTeam="gameInitialData?.visitorTeam"
+    />
+    <div class="sidebars-court">
+      <RefereeGameCallSidebar
+        v-if="localTeamCall && gameInitialData?.localTeam"
+        :team="gameInitialData.localTeam"
+        :call="localTeamCall"
+        @unlocked="getGameInitialData"
+      />
+      <div class="easy-referee-game-arbitrate-content">
+        <h1 class="text-center">CONTENT</h1>
+      </div>
+      <RefereeGameCallSidebar
+        v-if="visitorTeamCall && gameInitialData?.visitorTeam"
+        :team="gameInitialData.visitorTeam"
+        :call="visitorTeamCall"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'RefereeGameArbitrate',
+}
+</script>
---
+++ components/Referee/Game/Team/SetsWon.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { Team } from '@/domain/game'
+
+const props = defineProps({
+  localTeam: {
+    type: Object as PropType<Team>,
+    required: true,
+  },
+  visitorTeam: {
+    type: Object as PropType<Team>,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="easy-game-teams-sets-won-component">
+    <div class="teams-sets-won-team">
+      <div class="team-name">
+        {{ localTeam.name }}
+      </div>
+      <div class="sets-count">0</div>
+    </div>
+    <div class="teams-sets-won-team">
+      <div class="team-name">
+        {{ visitorTeam.name }}
+      </div>
+      <div class="sets-count">0</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'RefereeGameTeamSetsWon',
+}
+</script>
