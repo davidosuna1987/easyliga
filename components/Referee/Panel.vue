c9f6d3d1334411eaf89f12d55d37ed97fd81584c
David Osuna <davidosuna1987@gmail.com>
2023-08-17 11:03:46 +0200

    feat: moved index logic to a component

---
+++ components/Referee/Panel.vue
@@ -0,0 +1,61 @@
+<script lang="ts" setup>
+import { useAuthStore } from '@/stores/useAuthStore'
+import GameService from '@/services/game'
+import { Game, mapApiGameToGame } from '@/domain/game'
+
+const auth = useAuthStore()
+const gameService = new GameService()
+
+const currentGames = ref<Game[]>()
+const loadingApi = ref<boolean>(false)
+
+const getCurrentGames = async () => {
+  if (!auth.user) return
+
+  loadingApi.value = true
+  const { data } = await gameService.fetch({
+    where: `referee_id:${auth.user.id},status:!=:finished`,
+  })
+
+  currentGames.value = data.value?.data.games.map(game =>
+    mapApiGameToGame(game),
+  )
+
+  loadingApi.value = false
+}
+
+onBeforeMount(getCurrentGames)
+</script>
+
+<template>
+  <div
+    class="easy-referee-panel-component flex flex-col h-full"
+    :class="{ 'justify-between': !loadingApi && !currentGames?.length }"
+  >
+    <Heading class="mb-5" position="center">
+      {{
+        auth.profile?.first_name
+          ? $t('referees.welcome', { name: auth.profile.first_name })
+          : $t('referees.welcome_no_name')
+      }}
+    </Heading>
+    <LoadingLabel
+      v-if="loadingApi"
+      size="1.25rem"
+      :label="$t('games.loading', 2)"
+      class="flex justify-center"
+    />
+    <template v-else>
+      <RefereeCurrentGames v-if="currentGames?.length" :games="currentGames" />
+      <div v-else class="content">
+        <RefereeGameCreateButton />
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'RefereePanel',
+}
+</script>
--- pages/referee/index.vue
+++ pages/referee/index.vue
@@ -1,71 +1,14 @@
 <script lang="ts" setup>
-import { useAuthStore } from '@/stores/useAuthStore'
-import GameService from '@/services/game'
-import { Game, mapApiGameToGame } from '@/domain/game'
-
 definePageMeta({
   middleware: ['role'],
   roles: ['referee'],
 })
-
-const auth = useAuthStore()
-const gameService = new GameService()
-
-const currentGames = ref<Game[]>()
-const loadingApi = ref<boolean>(false)
-
-const getCurrentGames = async () => {
-  if (!auth.user) return
-
-  loadingApi.value = true
-  const { data } = await gameService.fetch({
-    where: `referee_id:${auth.user.id},status:!=:finished`,
-  })
-
-  currentGames.value = data.value?.data.games.map(game =>
-    mapApiGameToGame(game),
-  )
-
-  loadingApi.value = false
-}
-
-onBeforeMount(getCurrentGames)
 </script>
 
 <template>
   <NuxtLayout name="default">
-    <div
-      class="easy-referee-page flex flex-col"
-      :class="{ 'justify-between': !loadingApi && !currentGames?.length }"
-    >
-      <Heading class="mb-5" position="center">
-        {{
-          auth.profile?.first_name
-            ? $t('referees.welcome', { name: auth.profile.first_name })
-            : $t('referees.welcome_no_name')
-        }}
-      </Heading>
-      <LoadingLabel
-        v-if="loadingApi"
-        size="1.25rem"
-        :label="$t('games.loading', 2)"
-        class="flex justify-center"
-      />
-      <template v-else>
-        <RefereeCurrentGames
-          v-if="currentGames?.length"
-          :games="currentGames"
-        />
-        <div v-else class="content">
-          <RefereeGameCreateButton />
-        </div>
-      </template>
+    <div class="easy-referee-page">
+      <RefereePanel />
     </div>
   </NuxtLayout>
 </template>
-
-<script lang="ts">
-export default {
-  name: 'RefereePage',
-}
-</script>
