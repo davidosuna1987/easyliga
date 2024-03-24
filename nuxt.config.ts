d65b86202ac0f7274f95d635d0fcd81c49256b5d
David Osuna <davidosuna1987@gmail.com>
2024-03-24 07:08:55 +0100

    chore: remove nuxt loading

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  spaLoadingTemplate: false,
   modules: [
     '@pinia/nuxt',
     '@nuxtjs/i18n',
