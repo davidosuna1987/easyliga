f6f6697b859d57610e7665002439e8f1916d5279
David Osuna <davidosuna1987@gmail.com>
2024-07-02 00:00:41 +0200

    refactor: update gaps

--- components/Club/List.vue
+++ components/Club/List.vue
@@ -58,7 +58,7 @@ onMounted(getAuthuserClubWithSedesAndTeams)
   <div class="easy-club-teams-component">
     <Loading v-if="loadingApi" />
     <template v-else>
-      <EasyGrid v-if="clubManagesAnyTeam" :gap="2">
+      <EasyGrid v-if="clubManagesAnyTeam">
         <div class="club" v-for="club in clubsWithTeams">
           <header class="header flex justify-between">
             <Heading tag="h5" class="club__name">{{ club.name }}</Heading>
@@ -70,11 +70,11 @@ onMounted(getAuthuserClubWithSedesAndTeams)
             />
           </header>
           <template v-for="sede in club.sedes">
-            <EasyGrid v-if="sedeContainsTeams(sede)" class="sede mt-3" :gap="2">
+            <EasyGrid v-if="sedeContainsTeams(sede)" class="sede mt-3">
               <Heading tag="h6">{{ sede.name }}</Heading>
               <div
                 v-for="team in sede.teams"
-                class="team mt-2 p-3 rounded-lg"
+                class="team p-3 rounded-lg"
                 @click.prevent="goToEditClubTeam(club, team)"
               >
                 <div class="team-info flex items-center">
