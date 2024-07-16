8de20059dc3098f65a1f4e41e13ac7fcb2383a48
David Osuna <davidosuna1987@gmail.com>
2024-07-16 06:56:04 +0200

    feat: auth user can update federation

--- components/Federation/Edit.vue
+++ components/Federation/Edit.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
-import { Federation, mapApiFederationToFederation } from '@/domain/federation'
+import { useAuthStore } from '@/stores/useAuthStore'
 import FederationService from '@/services/federation'
+import { Federation, mapApiFederationToFederation } from '@/domain/federation'
 
 const { t } = useI18n()
+const auth = useAuthStore()
 const route = useRoute()
 const toast = useEasyToast()
 const federationService = new FederationService()
@@ -16,17 +18,34 @@ const getFederation = async () => {
   const { data, error } = await federationService.get(
     Number(route.params.federationId),
     {
-      with: 'federations.clubs.teams,leagues,clubs.teams,responsible.profile,address,licenses',
+      with: 'federation,federations.clubs.teams,leagues,clubs.teams,responsible.profile,address,licenses',
     },
   )
 
   if (error.value) {
     toast.mapError(Object.values(error.value?.data?.errors), false)
   } else if (data.value) {
-    federation.value = mapApiFederationToFederation(data.value.data.federation)
+    const f = mapApiFederationToFederation(data.value.data.federation)
+    if (!authUserCanUpdateFederation(f)) {
+      toast.error(t('errors.not_allowed_to_edit_federation'))
+      navigateTo('/')
+    } else {
+      federation.value = f
+    }
   }
 
   loadingApi.value = false
+}
+
+const authUserCanUpdateFederation = (federation: Federation) => {
+  if (!auth.user) return false
+
+  if (auth.hasAnyRole(['admin', 'staff'])) return true
+
+  return (
+    federation.responsibleId === auth.user.id ||
+    federation.federation?.responsibleId === auth.user.id
+  )
 }
 
 const setInitialFederation = async () => {
