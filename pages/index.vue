cdf4ea21d4f006a9e8ea941ba71f6b00f9f1ccfc
David Osuna <davidosuna1987@gmail.com>
2024-12-11 06:08:06 +0100

    refactor: prevent showing landing until it's created

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,28 @@ import { useAuthStore } from '@/stores/useAuthStore'
 
 useEasyHead('pages.index.title')
 
+const auth = useAuthStore()
 const { t } = useI18n()
+
+const showPage = computed(() => auth.isLoggedIn && auth.isAdmin())
+
+const redirectIfNotAdmin = () => {
+  if (!showPage.value) {
+    auth.loginRedirect()
+  }
+}
+
+onMounted(() => redirectIfNotAdmin())
 </script>
 
 <template>
   <NuxtLayout name="default">
-    <div class="easy-index-page">
-      <Heading class="mb-5" tag="h3" position="center">
-        {{ t('pages.index.welcome') }}
-      </Heading>
+    <div v-if="showPage" class="easy-index-page">
+      <template v-if="auth.isAdmin()">
+        <Heading class="mb-5" tag="h3" position="center">
+          {{ t('pages.index.welcome') }}
+        </Heading>
+      </template>
     </div>
   </NuxtLayout>
 </template>
